f81c36f6dd0d7ab64063bc23f0bc7ce3742b231e
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2020-12-27 21:41:01 +0100

    Update node.h

--- node.h
+++ node.h
@@ -1,22 +1,26 @@
+#ifndef EIG_NODE_H
+#define EIG_NODE_H
+
+
 #include <math.h>
 
 
 
 struct Node {
-  /*
-  
-  An instance of this class represents a node of the graph.
-  
-  */
-  
-  
-  int id, x, y, open, close;      // Id, x-position, y-position, opening time, closing time
-  
-  Node (int,int,int,int,int);     // Constructor
-  ~Node();                        // Destructor
-  
-  int operator-(const Node&);     // Distance between two nodes is calculated with this operator
-  
+    /*
+
+    An instance of this class represents a node of the graph.
+
+    */
+
+
+    int id, x, y, open, close;      // Id, x-position, y-position, opening time, closing time
+
+    Node (int,int,int,int,int);     // Constructor
+    ~Node();                        // Destructor
+
+    int operator-(const Node&);     // Distance between two nodes is calculated with this operator
+
 };
 
 
@@ -28,6 +32,10 @@ Node::~Node(){}
 
 
 // Euclidean distance with another node
-int Node::operator- (const Node& other) {
-  return (int) pow(pow(x - other.x, 2) + pow(y - other.y, 2), 0.5);
+int Node::operator-(const Node& other) {
+    return (int) pow(pow(x - other.x, 2) + pow(y - other.y, 2), 0.5);
 }
+
+
+
+#endif //EIG_NODE_H
