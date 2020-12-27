a3d28f4addecde63f44a2bd796a13b4d76a8de6b
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2020-12-27 21:10:33 +0100

    Update node.h

--- node.h
+++ node.h
@@ -1,3 +1,33 @@
-class Node {
+#include <math.h>
 
+
+
+struct Node {
+  /*
+  
+  An instance of this class represents a node of the graph.
+  
+  */
+  
+  
+  int id, x, y, open, close;      // Id, x-position, y-position, opening time, closing time
+  
+  Node (int,int,int,int,int);     // Constructor
+  ~Node();                        // Destructor
+  
+  int operator-(const Node&);     // Distance between two nodes is calculated with this operator
+  
 };
+
+
+// Constructor
+Node::Node (int id, int x, int y, int open, int close) : id(id), x(x), y(y), open(open), close(close){}
+
+// Destructor
+Node::~Node(){}
+
+
+// Euclidean distance with another node
+int Node::operator- (const Node& other) {
+  return (int) pow(pow(x - other.x, 2) + pow(y - other.y, 2), 0.5);
+}
