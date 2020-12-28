1129ba43ebcb1b18aad9a922434fec20f13dbbae
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2020-12-28 20:34:04 +0100

    Add files via upload

--- cc/node.h
+++ cc/node.h
@@ -2,7 +2,7 @@
 #define EIG_NODE_H
 
 
-#include <math.h>
+#include <cmath>
 
 
 
@@ -17,7 +17,7 @@ struct Node {
     int id, x, y, open, close;            // Id, x-position, y-position, opening time, closing time
 
     Node (int,int,int,int,int);           // Constructor
-    ~Node();                              // Destructor
+    ~Node() = default;                              // Destructor
 
     int operator-(const Node&) const;     // Distance between two nodes is calculated with this operator
 
@@ -26,9 +26,6 @@ struct Node {
 
 // Constructor
 Node::Node (int id, int x, int y, int open, int close) : id(id), x(x), y(y), open(open), close(close){}
-
-// Destructor
-Node::~Node(){}
 
 
 // Euclidean distance with another node
