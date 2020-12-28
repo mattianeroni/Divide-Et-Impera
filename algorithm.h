2d74520799e200dcaf8c19f3109e54b223594c34
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2020-12-28 10:41:04 +0100

    Update algorithm.h

--- algorithm.h
+++ algorithm.h
@@ -2,8 +2,10 @@
 #define EIG_ALGORITHM_H
 
 #include "node.h"
+#include <vector>
 #include <array>
 
+using std::vector;
 using std::array;
 
 // Template the number of nodes
@@ -21,19 +23,26 @@ class Algorithm {
 
 protected:
     long cost;              // The cost of the best solution found
-    array<Node, N> best;    // The best solution found so far
+    vector<Node> best;      // The best solution found so far
 
     // Set the best solution and its cost
-    virtual void set_best (array<Node,N>, array<array<long,N>,N>);
+    virtual void set_best (vector<Node>, long cost);
+    
+    // Get the cost of a solution
+    virtual long costify (vector<Node>, array<array<Node,N>,N>);
 };
 
 
+void Algorithm<N>::set_best (vector<Node> sol, long cost) {// array<array<long,N>,N> dists) {
+    (*this).best = sol; (*this).cost = cost;
+}
 
-template <std::size_t N>
-void Algorithm<N>::set_best (array<Node,N> sol, array<array<long,N>,N> dists) {
-    best = sol; cost = 0;
+template<std::size_t N>
+long Algorithm<N>::costify (vector<Node> sol, array<array<long,N>,N> dists) {
+    long cost = 0;
     for (int i = 0; i < sol.size() - 1; ++i)
-        cost += dists[sol[i].id][sol[i+1].id];
+        cost += dists[sol[i]][sol[i+1]];
+    return cost;
 }
 
 #endif //EIG_ALGORITHM_H
