5796671f3e525dd6017dd82c45452c858e5946e8
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2021-01-01 01:09:35 +0100

    Update dei.h

--- cc/dei.h
+++ cc/dei.h
@@ -7,6 +7,8 @@
 #include "node.h"
 #include "algorithm.h"
 
+#define self (*this)
+
 using std::array;
 using std::vector;
 
@@ -20,12 +22,13 @@ struct DivideEtImpera {
 
     array<array<long,N>,N> dists;                           // The distance matrix
     vector<Node> solution;                                  // The current solution
+    Node& cnode;                                            // The current node
     long value, delay;                                      // The travel time and the cumulative delay of the
                                                             // current solution.
     Algorithm<N>* alg;                                      // The implemented algorithm
     int p, maxsplit;                                        // Parameters of the algorithm
 
-    DivideEtImpera(Algorithm<N>*, array<array<long,N>,N>, int=50, int=3000);  // Constructor
+    DivideEtImpera(Algorithm<N>*, array<array<long,N>,N>, const Node&, int=50, int=3000);  // Constructor
     ~DivideEtImpera() = default;                            // Destructor
 
     void operator()(vector<Node>);                          // The eecution of the algorithm
@@ -35,12 +38,13 @@ struct DivideEtImpera {
 
 
 template<std::size_t N>
-DivideEtImpera<N>::DivideEtImpera(Algorithm<N>* alg, array<array<long,N>,N> dists, int p, int maxsplit)
-: alg(alg), dists(dists), value(0), delay(0), p(p), maxsplit(maxsplit) {}
+DivideEtImpera<N>::DivideEtImpera(Algorithm<N>* alg, array<array<long,N>,N> dists, const Node& cnode, int p, int maxsplit)
+: alg(alg), dists(dists), value(0), delay(0), p(p), maxsplit(maxsplit), cnode(cnode) {}
 
 
 template<std::size_t N>
 void DivideEtImpera<N>::operator()(vector<Node> tour){
+    tour.erase(std::find(tour.begin(), tour.end(), cnode));
     if (tour.size() > p) {
         vector<Node> f, s;
         int split = 0;
@@ -72,7 +76,9 @@ void DivideEtImpera<N>::operator()(vector<Node> tour){
 
 template<std::size_t N>
 void DivideEtImpera<N>::solve(vector<Node> tour){
-
+    alg->exe(cnode, value, tour, dists);
+    self.solution.insert(self.solution.end(),alg->best.begin(),alg->best.end());
+    self.value = alg->value; self.delay += alg->delay;
 }
 
 
